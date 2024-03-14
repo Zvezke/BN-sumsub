dce025e8534a5e09206c8f34fe72afc21cb8d1d5
Zvezke <superur@gmail.com>
2024-03-14 19:55:38 +0100

    Fix typo in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-neutral-900">
-      <h1 className="font-roboto text-2xl font-bold text-neutral-200">Code!</h1>
+      <h1 className="font-roboto text-2xl font-bold text-neutral-200">Code</h1>
     </main>
   );
 }
